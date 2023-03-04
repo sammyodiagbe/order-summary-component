8158819f620c6ed997cf4b6860bfdfce27174567
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-04 07:42:14 -0600

    done

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import MusicIcon from "./components/music_icon.svg";
 
 function App() {
   return (
-    <div className="component-container">
-      <header className="header"></header>
-      <main className="main">
-        <h1 className="big-title">Order Summary</h1>
-        <p className="sub">
-          You can now listen to millions of songs, audiobooks, and podcasts on
-          any device anywhere you like
-        </p>
-        <div className="plan">
-          <span className="music-icon">
+    <div className="container">
+      <div className="component-container">
+        <header className="header"></header>
+        <main className="main">
+          <h1 className="big-title">Order Summary</h1>
+          <p className="sub">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like
+          </p>
+          <div className="plan">
             <MusicIcon />
-          </span>
-          <div>
-            <h3>Annual Plan</h3>
-            <p>$59.99/year</p>
+            <div>
+              <h4>Annual Plan</h4>
+              <p>$59.99/year</p>
+            </div>
+            <a href="#change">change</a>
           </div>
-          <a href="#change">change</a>
-        </div>
 
-        <button className="btn">Proceed to payment</button>
-        <button className="btn">Cancel order</button>
-      </main>
+          <button className="btn">Proceed to payment</button>
+          <button className="btn cancel">Cancel order</button>
+        </main>
+      </div>
     </div>
   );
 }
